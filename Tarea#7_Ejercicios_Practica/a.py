90b84abc7193c65620113c593839876ff73f0570
axel <axel.aatl@gmail.com>
2023-02-18 19:26:59 -0600

    Tarea #7 - Ejercicios de Practica

---
+++ Tarea#7_Ejercicios_Practica/a.py
@@ -0,0 +1,5 @@
+# Ejercicios de Práctica
+# Autor: Axel Lopez
+
+str_nombre = input("Ingresa tu nombre: ")
+print("¡Bienvenido %s!" %(str_nombre))
---
+++ Tarea#7_Ejercicios_Practica/b.py
@@ -0,0 +1,29 @@
+# Ejercicios de Práctica
+# Autor: Axel Lopez
+
+int_ancho = 17
+float_alto = 12.0
+float_R = 0
+
+#1
+float_R = int_ancho / 2
+print("1) ancho/2, Resultado: %1.2f" %(float_R))
+print(type(int_ancho / 2))
+print(" ")
+
+#2
+float_R = int_ancho / 2.0
+print("2) ancho/2.0, Resultado: %1.2f" %(float_R))
+print(type(int_ancho / 2.0))
+print(" ")
+
+#3
+float_R = float_alto / 3
+print("3) alto/3, Resultado: %1.2f" %(float_R))
+print(type(float_alto / 3))
+print(" ")
+
+#4
+int_R = 1 + 2 * 5
+print("4) 1 + 2 * 5, Resultado: %d" %(int_R))
+print(type(1 + 2 * 5))
